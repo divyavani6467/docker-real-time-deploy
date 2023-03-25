7821bca794a5e81507ee6aa75f12aa32b1e67ab4
divya <you@example.com>
2023-03-25 14:42:49 +0000

    docker commits

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  amazon-linux-extras install java-openjdk11 -y
+  yum install git jenkins docker -y
+  systemctl start jenkins
+  systemctl status jenkins
+ 
